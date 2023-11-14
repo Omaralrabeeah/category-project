4a3641aaf1e89143762c79b43235fcf8eb9e7704
Omaralrabeeah <omar.alrabeeah@gmail.com>
2023-11-14 18:39:21 +0300

    done

--- src/api/auth.js
+++ src/api/auth.js
@@ -1,17 +1,35 @@
+import { jwtDecode } from "jwt-decode";
 import instanse from ".";
 
 const login = async (userInfo) => {
-  const res = await instanse.post("", userInfo);
-  return res.data;
+  const { data } = await instanse.post("/api/users/signin", userInfo);
+  storeToken(data?.token);
+  return data;
 };
 
 const register = async (userInfo) => {
   const formData = new FormData();
-  for (let key in userInfo) {
-    formData.append(key, userInfo[key]);
-  }
-  const res = await instanse.post("", userInfo);
-  return res.data;
+  for (const key in userInfo) formData.append(key, userInfo[key]);
+  const { data } = await instanse.post("/api/users/signup", formData);
+  storeToken(data?.token);
+
+  return data;
+};
+const storeToken = (token) => {
+  localStorage.setItem("token", token);
+};
+const checkToken = () => {
+  const token = localStorage.getItem("token");
+  if (token) {
+    const decode = jwtDecode(token);
+    const currentTime = Date.now() / 1000;
+    if (decode.exp < currentTime) {
+      localStorage.removeItem("token");
+    } else return false;
+  } else return false;
+};
+const logOut = () => {
+  localStorage.removeItem("token");
 };
 
-export { login, register };
+export { login, register, storeToken, checkToken, logOut };
---
+++ src/components/Navbar.js
@@ -0,0 +1,80 @@
+import React, { useContext } from "react";
+import { Link, NavLink, useNavigate } from "react-router-dom";
+import UserContext from "../context/UserContext";
+import { logOut } from "../api/auth";
+import { useMutation } from "@tanstack/react-query";
+
+const Navbar = () => {
+  let [user, setUser] = useContext(UserContext);
+  const navigate = useNavigate();
+  const handleLogout = () => {
+    logOut();
+    setUser(false);
+    navigate("/");
+  };
+
+  // const { mutate: handleLogout } = useMutation({
+  //   mutationKey: ["logout"],
+  //   mutationFn: () => logOut(),
+  //   onSuccess: () => {
+  //     setUser = false;
+  //     navigate("/");
+  //   },
+  // });
+
+  return (
+    <nav className="bg-gray-800">
+      <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
+        <div className="flex items-center justify-between h-16">
+          <div className="flex items-center">
+            <Link to="/">
+              <span className="font-semibold text-xl text-white">HOME</span>
+            </Link>
+          </div>
+          <div className="block">
+            <div className="ml-10 flex items-baseline space-x-4">
+              <NavLink
+                to="/transactions"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Transactions
+              </NavLink>
+              <NavLink
+                to="/users"
+                className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+              >
+                Users
+              </NavLink>
+
+              {user ? (
+                <button
+                  onClick={handleLogout}
+                  className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                >
+                  Logout
+                </button>
+              ) : (
+                <>
+                  <NavLink
+                    to="/login"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Login
+                  </NavLink>
+                  <NavLink
+                    to="/register"
+                    className="text-gray-300 hover:bg-gray-700 hover:text-white px-3 py-2 rounded-md text-sm font-medium"
+                  >
+                    Register
+                  </NavLink>
+                </>
+              )}
+            </div>
+          </div>
+        </div>
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
