92ead448302058de62340fc1dadf5cd9a28eb514
tahanifayez <144371267+tahanifayez@users.noreply.github.com>
2023-11-15 21:34:44 +0300

    done

--- src/pages/Category.js
+++ src/pages/Category.js
@@ -2,8 +2,8 @@ import React from "react";
 
 const Category = () => {
   return (
-    <div className="w-screen h-screen bg-yellow-100 bg-cover bg-center ...">
-      <div className="relative">
+    <div className="w-screen h-screen  ">
+      {/* <div className="relative">
         <img
           src="https://mytastycurry.com/wp-content/uploads/2023/05/Amritsari-Paneer-Bhurji-.jpg"
           class=" absolute h-500 w-500 absolute top-0 shadow-2xl ...bg-indigo-500 opacity-75 ..."
@@ -28,6 +28,36 @@ const Category = () => {
           <p className="font-mono  font-bold text-3xl ... text-yellow-900 left-30">
             KUWAITI
           </p>
+        </div>
+      </div> */}
+      <div className="flex flex-col w-full h-full gap-3">
+        <div className=" flex justify-center items-center gap-5  h-[10%] w-full ">
+          <input
+            type="text"
+            placeholder="Search"
+            className="input input-bordered w-full max-w-xs"
+          />
+          <button className="btn btn-neutral">Search</button>
+        </div>
+        <div className="h-[90%] w-full  flex flex-wrap gap-5 justify-center">
+          {/* START CARD */}
+          <div className="card card-compact w-96 bg-base-100 shadow-xl h-[400px]">
+            <figure>
+              <img
+                src="/images/stock/photo-1606107557195-0e29a4b5b4aa.jpg"
+                alt="Shoes"
+              />
+            </figure>
+            <div className="card-body">
+              <h2 className="card-title">Shoes!</h2>
+              <p>If a dog chews shoes whose shoes does he choose?</p>
+              <div className="card-actions justify-end">
+                <button className="btn btn-primary">Buy Now</button>
+              </div>
+            </div>
+          </div>
+
+          {/* END CARD */}
         </div>
       </div>
     </div>
